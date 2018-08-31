0416418c3e96ce2cba8a9103bd50dd72d65e5da9
Soumya.Hosamani <soumya.hosamani@causeway.com>
2018-08-31 19:03:56 +0530

    WRiting tests
    WRiting tests

---
+++ TradeSimulatorTests/TradeAutomationTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TradeSimulator;
+using System.Collections.Concurrent;
+namespace TradeAutomationTests
+{
+
+
+    [TestClass]
+    public class TradeSimulationTests
+    {
+        [TestMethod]
+        public void ExecuteStrategyTest()
+        {
+            // create strategy
+            IStrategy testStrategy = new TestStrategy() { Name = "Test", Symbol = "Symbol" };
+
+            // create datafeed
+            IFeedProvider feedProvider = new TestFeedProvider();
+
+            DataFeed feed = new DataFeed(feedProvider, new FeedQueue<Tick>());
+            feed.Start();
+
+            // subscribe to feed
+            feed.Subscribe(testStrategy);
+
+            // ensure strategy is running.. ??? 
+            var purchasedTicks = testStrategy.BoughtTicks. ///i need this 
+
+
+            feed.Stop();                        
+        }
+    }
+
+    public class TestStrategy : IStrategy
+    {
+         public string Name
+        {
+            get; set;
+        }
+
+        public string Symbol
+        {
+            get; set;
+        }
+
+        public void OnTick(Tick tick)
+        {
+            if (tick.Price > 1)
+                Buy(tick);
+        }
+
+        
+    }
+
+    public class TestFeedProvider : IFeedProvider
+    {
+        public event NewTickEventHandler NewTickEvent;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe(string Symbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
